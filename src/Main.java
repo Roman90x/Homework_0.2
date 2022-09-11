d0c95e3b96178c425b278f373036711b5f74cf4f
ROMAN <roman90x@icloud.com>
2022-09-11 20:43:16 +0600

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Переменные.Часть 1");
 
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
@@ -14,8 +14,12 @@ public class Main {
         System.out.println(cat);
         paper = paper + 4;
         System.out.println(paper);
-
-
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
 
     }
 
